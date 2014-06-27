2daea5b1928bd44e38dbbef21cca6fa3fa5bd22b
DTSCode <DTSCode@gmail.com>
2014-06-27 10:15:23 -0700

    added grammar for language

--- main.cpp
+++ main.cpp
@@ -3,60 +3,36 @@
 #include   <sstream>
 #include <exception>
 
+namespace Token {
+    enum Token {
+        
+    };
+}
+
 int main(int argc, char *argv[]) {
-    try {
-        static std::istream *Stream;
+    static std::istream *Stream;
 
-        switch(argc) {
-            case 1:
-                Stream = &std::cin;
+    switch(argc) {
+        case 1:
+            Stream = &std::cin;
+            break;
+
+        case 2:
+            Stream = new std::ifstream(argv[1]);
+            break;
+
+        case 3:
+            if(argv[2] == "-c") {
+                Stream = new std::istringstream(argv[3]);
                 break;
+            }
 
-            case 2:
-                Stream = new std::ifstream(argv[1]);
-                break;
-
-            case 3:
-                if(argv[2] == "-c") {
-                    Stream = new std::istringstream(argv[3]);
-                    break;
-                }
-
-            default:
-                std::cerr<<"error: invalid arguments."<< std::endl;
-                return 0;
-        }
+        default:
+            std::cerr<<"error: invalid arguments."<< std::endl;
+            return 0;
     }
 
-    catch(std::bad_alloc &Error) {
-        std::cerr<< Error.what() << std::endl;
-    }
-
-    catch(const std::bad_exception &Error) {
-        std::cerr<< Error.what() << std::endl;
-    }
-
-    catch(const std::invalid_argument &Error) {
-        std::cerr<< Error.what() << std::endl;
-    }
-
-    catch(const std::length_error &Error) {
-        std::cerr<< Error.what() << std::endl;
-    }
-
-    catch(const std::out_of_range &Error) {
-        std::cerr<< Error.what() << std::endl;
-    }
-
-    catch(const std::overflow_error &Error) {
-        std::cerr<< Error.what() << std::endl;
-    }
-
-    catch(const std::range_error &Error) {
-        std::cerr<< Error.what() << std::endl;
-    }
-
-    catch(const std::underflow_error &Error) {
-        std::cerr<< Error.what() << std::endl;
+    if(Stream != &std::cin) {
+        delete Stream;
     }
 }
